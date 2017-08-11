5fef11f801831508c71d32496c97e8163ccd1370
Adem Yenice <yenicead@itu.edu.tr>
2017-08-11 11:28:16 +0300

    Code Update
    I changed the code, add comments, also updated readme file.

--- README.md
+++ README.md
@@ -8,12 +8,21 @@ For Windows :<br>
 - Create a txt file and copy all the code, paste it in the txt file and save it as a find_price.py file under C:\ directory.<br>
 - To run the python code you need python so go to website : https://www.python.org/downloads/ download and install python.<br>
 - After installation, open a command prompt (cmd) and enter like this:<br>
-      C:\>Python34\python.exe C:\find_price.py<br>
+    C:\>Python34\python.exe C:\find_price.py<br>
       
 For Linux :<br>
 - First, you need to check if python is installed. To learn it type : <br>
     python --version <br>
 - If it is installed then you will see a verion of python. Otherwise, you need to install it. For installation type : <br>
     apt-get install python <br>
+- You also need to install 'beautifulsoup4' library. <br>
+	pip install beautifulsoup4 <br>
 - After installation, you need to create a file 'find_price.py' via vim and copy the code, paste it then save this file.<br> Then run the following code :<br>
-    python2.7 find_price.py<br>
+    python2.7 find_price.py <br>
+
+
+For Python2.x.x versions : <br>
+- You need to change the 'input' to 'raw_input' otherwise you will get error.<br>
+- Goto line 7 <br>
+- Change the 'input' with 'raw_input'. So it will look like: <br>
+	URL = raw_input('Enter an URL : ')
--- hepsiburadaPriceFinder.py
+++ hepsiburadaPriceFinder.py
@@ -1,23 +1,28 @@
 ﻿from bs4 import BeautifulSoup
 import requests, re
 
-# Taking the URL from User
-URL = input('Enter an URL : ')
-URLIndex = requests.get(URL)
+# Taking the URL from user until given URL is valid.
+while True:
+     try:
+          URL = input('Enter an URL : ')
+          URLIndex = requests.get(URL)
+          break
+     except ValueError:
+          print('Oops!  That was no valid URL.  Try again...');
 
 # Using bs4 library, we pull the content of the url.
 soup = BeautifulSoup(URLIndex.content, "html.parser")
 
 # This method is used to find the price of the product with related parameters.
 priceFinder = lambda tag: (getattr(tag, 'name', None) == 'span' and 'content' in tag.attrs and '' 
-                          in tag.get_text().lower())
+                       in tag.get_text().lower())
 
 # After taking the span tags, we first convert it to str then split every words.
-abc = str(soup.find(priceFinder)).split(" ")
+pfString = str(soup.find(priceFinder)).split(" ")
 
 # The third [2] information includes the price of the product. We also need to split the price and content tags. 
-b = str(abc[2]).split("=", 2)
+productInformation = str(pfString[2]).split("=", 2)
 
 # Finally, we split the price of the product and showing to user.
-c = b[1]
-print('The price : ', c, 'TL')
+productPrice = productInformation[1]
+print('The price :', productPrice,  'TL');
